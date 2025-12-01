689c4eb7ee807264ada0441a7e3114b6a80598fe
Michael Köck <mkoeck@elektroshopkoeck.com>
2025-12-01 16:23:33 +0100

    [FIX] connector: context is readonly recreate env

--- component/tests/common.py
+++ component/tests/common.py
@@ -40,9 +40,9 @@ class ComponentMixin:
             current_addon = _get_addon_name(cls.__module__)
             env["component.builder"].load_components(current_addon)
             if hasattr(cls, "env"):
-                cls.env.context = dict(
-                    cls.env.context, components_registry=cls._components_registry
-                )
+                cls.env = cls.env(context=dict(
+                    **cls.env.context, components_registry=cls._components_registry
+                ))
 
     # pylint: disable=W8106
     def setUp(self):
@@ -76,9 +76,10 @@ class TransactionComponentCase(common.TransactionCase, ComponentMixin):
         common.TransactionCase.setUp(self)
         ComponentMixin.setUp(self)
         # There's no env on setUpClass of TransactionCase, must do it here.
-        self.env.context = dict(
-            self.env.context, components_registry=self._components_registry
-        )
+        
+        context = dict(**self.env.context)
+        context.update(components_registry=self._components_registry)
+        self.env = self.env(context=context)
 
 
 class ComponentRegistryCase:
